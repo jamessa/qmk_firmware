b64a1988c41f182150cc91dfa18bd362fea410b5
james sa <james@keto.tw>
2020-07-10 21:29:20 +0800

    ime toggle

--- keyboards/charming/keymaps/default/keymap.c
+++ keyboards/charming/keymaps/default/keymap.c
@@ -19,6 +19,7 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
+    _COLEMAKDH,
     _FN
 };
 
@@ -31,19 +32,17 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
+          KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     KC_BSLS,
+        KC_CAPS,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,       KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,  KC_QUOT,
+      KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   TO(_COLEMAKDH),  KC_N,   KC_M,   KC_COMM,KC_DOT,  KC_SLSH, KC_RSHIFT,
+              MO(_FN),KC_LCTL, KC_LALT,KC_LGUI,KC_BSPC,KC_ENT,   KC_SPC,   KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT
+    ),
+    [_COLEMAKDH] LAYOUT(
           KC_TAB,   KC_Q,   KC_W,   KC_F,   KC_P,   KC_B,   KC_J,   KC_L,   KC_U,   KC_Y,   KC_SCLN,   KC_BSLS,
         KC_CAPS,  KC_A,   KC_R,   KC_S,   KC_T,   KC_G,       KC_M,   KC_N,   KC_E,   KC_I,   KC_O,      KC_QUOT,
-      KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_D,   KC_V,   KC_NO,  KC_K,   KC_H,   KC_COMM,KC_DOT,  KC_SLSH, KC_RSHIFT,
+      KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_D,   KC_V,   TO(_BASE),  KC_K,   KC_H,   KC_COMM,KC_DOT,  KC_SLSH, KC_RSHIFT,
               MO(_FN),KC_LCTL, KC_LALT,KC_LGUI,KC_BSPC, KC_ENT,   KC_SPC,   KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT
     ),
-
-    // QWERTY
-    // [_BASE] = LAYOUT(
-    //       KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     KC_BSLS,
-    //     KC_CAPS,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,       KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,  KC_QUOT,
-    //   KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_NO,  KC_N,   KC_M,   KC_COMM,KC_DOT,  KC_SLSH, KC_RSHIFT,
-    //           MO(_FN),KC_LCTL, KC_LALT,KC_LGUI,KC_DEL,KC_ENT,   KC_SPC,   KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT
-    // ),
     [_FN] = LAYOUT(
         QMKBEST, QMKURL,  _______,
             RESET,    XXXXXXX,
