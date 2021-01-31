7184e199b24154ae30b070122e88c874451fb321
james sa <james@keto.tw>
2021-01-31 17:00:46 +0800

    puncuation layer

--- keyboards/gentleman/keymaps/debug/keymap.c
+++ keyboards/gentleman/keymaps/debug/keymap.c
@@ -48,9 +48,9 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] LAYOUT(
     KC_ESC, KC_GRV, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC,
-    KC_TAB, _A(KC_A),_C(KC_R),_S(KC_S),_G(KC_T),KC_G,    KC_QUOT,    KC_M, _G(KC_N),_S(KC_E),_C(KC_I),_A(KC_O),         KC_BSLS,
+    KC_TAB, _A(KC_A),_C(KC_R),_S(KC_S),_G(KC_T),KC_G,    KC_BSLS,    KC_M, _G(KC_N),_S(KC_E),_C(KC_I),_A(KC_O),         KC_QUOT,
     KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_MINS,  KC_EQL,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    _______,  _______,   _______, _______,  FN_BS,  PN_ENT,  KC_CAPS, KC_SPC,   KC_LEFT,    KC_DOWN,    KC_UP,  KC_RGHT
+    _______,  _______,   _______, _______,  FN_BS,  PN_ENT,  KC_CAPS,LT(_PN, KC_SPC),   KC_LEFT,    KC_DOWN,    KC_UP,  KC_RGHT
     ),
     // [_KAI] LAYOUT(
     //     KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,             KC_QUOT,          KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
@@ -64,6 +64,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //     _______, _______, _______, KC_QUOT, _______, _______, _______, _______, _______, KC_1,    KC_2,    KC_3,    KC_ASTR,
     //              _______, _______, _______, _______, _______, _______, _______, KC_0,    KC_COMM, KC_DOT,  KC_SLSH
     // ),
+    [_PN] LAYOUT(
+    _______, _______, KC_TILD, KC_ASTR, KC_AMPR, KC_LCBR, KC_RCBR, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_HASH, KC_AT, KC_EXLM, KC_LPRN, KC_RPRN, _______, _______, _______, _______, _______, _______, _______,
+    _______, KC_CIRC, KC_PERC, KC_DLR, KC_LBRC, KC_RBRC, KC_UNDS, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______,    _______,   _______, _______,   _______,   _______, _______, _______, _______
+    ),
     [_FN] LAYOUT(
     // _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     //     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -73,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, SCNSHOT,   DIVVY,_______, _______,                   _______, KC_7, KC_8, KC_9, KC_PLUS, _______, _______,
             _______,    UNDO,    CUT,   COPY,    PSTE,   _______, _______,          _______, KC_4, KC_5, KC_6, KC_MINS, _______,
             _______,    REDO,  WKSPLF,  MSNCTL,  WKSPRT, _______, _______,  _______,_______, KC_1, KC_2, KC_3, KC_ASTR, _______,
-                _______, _______, _______, _______,    _______,  _______, _______,   KC_0,   KC_COMM, KC_DOT, _______, KC_SLSH
+                _______, _______, _______, _______,    _______,  _______, _______,   KC_0,   KC_DOT,KC_COMM,KC_DOT, KC_SLSH
     //
     // _______, _______, _______, SCNSHOT,   DIVVY,   _______,   _______,    _______,    _______,    _______,    KC_UP,   KC_VOLD, KC_VOLU, _______, _______,
     //     _______,    UNDO,       CUT,    COPY,      PSTE,   _______,    _______,    _______,      KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
