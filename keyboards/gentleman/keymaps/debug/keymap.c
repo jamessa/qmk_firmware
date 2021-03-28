8b40b0d8c92806cf1fa8ad7ab1974620757fde78
James Sa <james.sa.2013@gmail.com>
2021-03-28 22:17:05 +0800

    typing mode

--- keyboards/gentleman/keymaps/debug/keymap.c
+++ keyboards/gentleman/keymaps/debug/keymap.c
@@ -21,7 +21,8 @@ enum layer_names {
     _BASE,  // Colemak mod DH
     _KAI,   // beginner, no modifier on home row
     _PN,    // puncuation
-    _FN     // function
+    _FN,    // function
+    _TYPE   // typing practice layer
 };
 
 #define _C(X) CTL_T(X)
@@ -56,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      // shift    Z        X        C        D        V        -        =        K        H        ,        .        /        shift
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_MINS, KC_EQL,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
      //                                            bkspc    enter     caps    space    left     down      up      right
-              _______, _______, _______, _______, FN_BS,   PN_ENT,  KC_CAPS, SPC_PN,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+      _______,  _______,   TO(_TYPE), TO(_FN),    FN_BS,   PN_ENT,  KC_CAPS, SPC_PN,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
     // [_KAI] LAYOUT(
     //     KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,             KC_QUOT,          KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
@@ -85,12 +86,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, SCNSHOT,   DIVVY, SPOTLT, _______,                   _______, KC_7, KC_8, KC_9, KC_PLUS, KC_PERC, _______,
             _______,    UNDO,    CUT,   COPY,    PSTE,   _______, _______,          _______, KC_4, KC_5, KC_6, KC_MINS, KC_ASTR,
             _______,    REDO,  WKSPLF,  MSNCTL,  WKSPRT, _______, _______,  _______,_______, KC_1, KC_2, KC_3, KC_SLSH, _______,
-                _______, _______, _______, _______,    _______,  _______, _______,   KC_0,   KC_DOT,_______,_______, _______
+                TO(_BASE),TO(_BASE),TO(_BASE),TO(_BASE), _______,  _______, _______,   KC_0,   KC_DOT,_______,_______, _______
     //
     // _______, _______, _______, SCNSHOT,   DIVVY,   _______,   _______,    _______,    _______,    _______,    KC_UP,   KC_VOLD, KC_VOLU, _______, _______,
     //     _______,    UNDO,       CUT,    COPY,      PSTE,   _______,    _______,    _______,      KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
     //             _______,    REDO,    WKSPLF,    MSNCTL,     WKSPRT,  _______, _______,_______,, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
     //              _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    ),
+    [_TYPE] LAYOUT(
+    KC_ESC, KC_GRV, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC,
+    KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,    KC_G,      KC_BSLS,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_MINS,  KC_EQL,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    TO(_BASE),TO(_BASE),TO(_BASE),TO(_BASE),  FN_BS,  PN_ENT,  KC_CAPS,LT(_PN, KC_SPC),   KC_LEFT,    KC_DOWN,    KC_UP,  KC_RGHT
     )
 };
 
