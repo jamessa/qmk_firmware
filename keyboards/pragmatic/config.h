fe0cc28ec187a2ceb76e9dc3f1671e43049dc6e9
James Sa <james.sa.2013@gmail.com>
2021-11-01 01:38:36 +0800

    pins ready

--- keyboards/pragmatic/config.h
+++ keyboards/pragmatic/config.h
@@ -27,8 +27,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT      pragmatic
 
 /* key matrix size */
-#define MATRIX_ROWS 2
-#define MATRIX_COLS 3
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 12
 
 /*
  * Keyboard Matrix Assignments
@@ -40,8 +40,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { D7, E6 }
-#define MATRIX_COL_PINS { D3, D2, D1 }
+#define MATRIX_ROW_PINS { D7, E6, B4, B5, B2, B6 }
+#define MATRIX_COL_PINS { D3, D2, D1, D0, D4, C6, B3, B1, F7, F6, F5, F4 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
